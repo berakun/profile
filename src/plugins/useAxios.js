fba82b28c2df90a17cce14d0bcfd3c284026e674
rifki <jumjum8421@gmail.com>
2023-03-24 16:32:13 +0700

    commit 24 maret 2023

--- src/plugins/useAxios.js
+++ src/plugins/useAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const gorilla = axios.create({
-  baseURL: 'https://crudcrud.com/api/39c7e241544445f4b20bf1a1778d576c/unicorns',
+  baseURL: 'http://localhost:8000/api/posts',
   timeout: 1000
   // headers: {
   //   Authorization: `Bearer FeUOw1LkQVnbXrZ8LozPtwVlumXI8ABfANNZfJaVBWg3_tPdzw`
