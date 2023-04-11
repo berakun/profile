4512fd745553e2fca7bbce9f14c52c015f74882d
rifki <jumjum8421@gmail.com>
2023-04-11 16:34:41 +0700

    commit 11 april 2023 tahap 3

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -4,4 +4,8 @@ import { ReactComponent as Linkedin } from './linkedin.svg';
 import { ReactComponent as Wa } from './wa.svg';
 import { ReactComponent as Downicon } from './downicon1.svg';
 
-export { Fb, Ig, Linkedin, Wa, Downicon };
+import { ReactComponent as Delete } from './delete.svg';
+import { ReactComponent as Pencil } from './pencil.svg';
+
+
+export { Fb, Ig, Linkedin, Wa, Downicon, Delete, Pencil };
--- src/components/adminComponents/pageadmin.jsx
+++ src/components/adminComponents/pageadmin.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-
+import { createElement, useEffect, useState } from "react";
+import { Delete, Pencil } from '../../assets/icon/index';
 import { gorilla } from "../../plugins/useAxios";
 
 function Pageadmin() {
 
 
+    //MENAMBAH DATA KE REST API
     const [form, setForm] = useState({ product: '', color: '', category: '', price: 0 })
 
     const handleInput = (key, e) => {
@@ -18,10 +19,13 @@ function Pageadmin() {
     const handleClick = async (e) => {
         e.preventDefault();
         const res = await gorilla.post('/store', form)
+        window.location.reload();
         console.log(res);
         loadData()
     }
 
+
+    //MENAMPILKAN DATA DARI REST API
     const [data, setData] = useState([])
 
     async function loadData() {
@@ -35,44 +39,33 @@ function Pageadmin() {
         loadData();
     }, []);
 
+
+    //POPUP
     const [showPopup, setShowPopup] = useState(false);
     const togglePopup = () => {
         setShowPopup(!showPopup);
     }
 
+
+    // SIMPAN DATA REST API KETIKA DI KLIK
+    const [selectedId, setSelectedId] = useState(null);
+    useEffect(() => {
+        fetch(gorilla)
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
+    const clickId = (id) => {
+        setSelectedId(id);
+    }
+
+
     return (
-
-
 
         <div className="rounded-lg relative overflow-x-auto container mx-auto py-10">
 
-
-            {/* 
-            <form onSubmit={handleClick}>
-                <div className="mb-6">
-                    <label for="input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">product name</label>
-                    <input type="input" onInput={(e) => handleInput('product_name', e)} value={form.product.current} id="product" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                </div>
-                <div className="mb-6">
-                    <label for="input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">color</label>
-                    <input type="input" onInput={(e) => handleInput('color', e)} value={form.color.current} id="color" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                </div>
-                <div className="mb-6">
-                    <label for="input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">category</label>
-                    <input type="input" onInput={(e) => handleInput('category', e)} value={form.category.current} id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                </div>
-                <div className="mb-6">
-                    <label for="input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">price</label>
-                    <input type="input" onInput={(e) => handleInput('price', e)} value={form.price.current} id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-            </form>
-
-
-            {JSON.stringify(data)} */}
-
-            <div onClick={togglePopup} className="pb-4 flex flex-row justify-end items-center">
-                <button className="rounded-lg py-2 px-4">
+            <div onClick={togglePopup} className="pb-1 flex flex-row justify-end items-center">
+                <button className="rounded-md py-2 px-4">
                     Add
                 </button>
             </div>
@@ -101,11 +94,14 @@ function Pageadmin() {
                 </div>
             ) : null}
 
-            {JSON.stringify(data)}
+            {/* {JSON.stringify(data)} */}
 
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
+                        <th scope="col" className="px-6 py-3">
+                            No
+                        </th>
                         <th scope="col" className="px-6 py-3">
                             Product name
                         </th>
@@ -118,13 +114,22 @@ function Pageadmin() {
                         <th scope="col" className="px-6 py-3">
                             Price
                         </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Delete
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(data => (
+                    {data.map((data, index) => (
                         <tr
                             key={data.id}
                             className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td className="px-6 py-4">
+                                {index + 1}
+                            </td>
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {data.product_name}
                             </th>
@@ -136,6 +141,16 @@ function Pageadmin() {
                             </td>
                             <td className="px-6 py-4">
                                 {data.price}
+                            </td>
+                            <td className="px-6 py-4 w-5 h-5" onClick={() => clickId(data.id)}>
+                                <button type="Submit" className="bg-inherit">
+                                    {createElement(Pencil)}
+                                </button>
+                            </td>
+                            <td className="px-6 py-4 w-5 h-5" onClick={() => clickId(data.id)}>
+                                <button type="Submit" className="bg-inherit">
+                                    {createElement(Delete)}
+                                </button>
                             </td>
                         </tr>
                     ))}
